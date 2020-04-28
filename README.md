6db818c01baa6eafc9a662102a602fb676010784
Charles McGuinness <charles@mcguinness.us>
2020-04-28 16:17:21 -0400

    Documentation

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# lwcsi
-A LWC Replacement for Selectable Items
+# lwcsi01
+
+## A LWC Replacement for Selectable Items
+
+This is a Lightning Web Component that is intended to be used inside of Vlocity's OmniScript to provide similar functionality to a selectable items element.  There are some differences, however, in its behavior, but the differences are minor and easy to adapt to.
+
+As a reminder, a Selectable Items behaves as a filter: you pass it an array of data (often a table), it displays the table and allows the user to select zero, one, or many rows (depending upon requirements), and the value of the element is the selected items.
+
+## Installing the LWC
+
+The LWC is comprised of three files:
+
+1. lwcsi01.html
+2. lwcsi01.js
+3. lwcsi01.js-meta.xml
+
+There are other files in this repository, but they are not to be deployed.
+
+One way to deploy this is to drag the directory holding the repository into Visual Studio Code under the lwc directory (and then deleting all but the above three files).
+
+## Using the LWC
+
+If you are familiar with Selectable Items, you will find this similar.  One important difference is that the source of the data can be different from the element itself; if you do that (which I recommend), it leaves the original data unmolested.
+
+Here's a sample configuration we'll walk through:
+
+![](/Users/charles/VlocityRepos/lwcsi01/readme01.png)
+
+The configuration is driven by the following custom properties:
+
+| Name    | Description                                                  |
+| ------- | ------------------------------------------------------------ |
+| headers | A list of the titles to display for the columns, in order    |
+| input   | The name of the data element holding the data to be displayed |
+| values  | The names of the columns to be displayed, in order           |
+| select  | optional, if "f" or "false", it will remove the select box from each row |
+
+While the source of the data can definitely be a DataRaptor or Integration Procedure, you can also use a Set Values to set dummy data.  For example:
+
+![](/Users/charles/VlocityRepos/lwcsi01/readme02.png)
+
+Because the structure of the data (an array of key:value pairs), you have to Edit as JSON to see the actual data:
+
+![](/Users/charles/VlocityRepos/lwcsi01/readme03.png)
+
+In this case, I've created a variable called "listdata" which has two rows.
+
+All this, together, produces the output that looks like (with select=true):
+
+![](/Users/charles/VlocityRepos/lwcsi01/readme04.png)
+
+You can see here the input, listdata, and the output, Step1.ExampleSI, in the Data JSON.
+
+## Notes and Caveats
+
+In order to test the LWC, even in preview, you have to activate your OmniScript.  This takes ~2 minutes (+/- quite a bit).  If the OmniScript is not activated, and you preview, this LWC will not render.  That's not harmful, except that it will be missing.  But be aware that testing with the LWC is going to be slow going.
---
+++ lwcsi01.js
@@ -0,0 +1,190 @@
+/**
+ * Generic "Selectable Items" Lightning Web Component for OmniScript
+ * 
+ * 
+ * @author Kirk Leibert <kleibert@vlocity.com>
+ * @author Charles McGuinness <cmcguinness@vlocity.com>
+ * 
+ */
+import { LightningElement, track, api } from 'lwc';
+
+import { OmniscriptBaseMixin } from 'vlocityins2/omniscriptBaseMixin';
+
+export default class lwcsi01 extends OmniscriptBaseMixin(LightningElement) {
+
+
+    @track data = {};
+    @track vals = [];
+    @track cols = [];
+    @track headers = [];
+    @track displayRows = [];
+    @api title = '';
+    @api multiselect;
+
+    @track parms_showSelect = true;
+    parms_headers = null;
+    parms_values = null;
+    parms_input = null;
+
+
+    /*  *****************************************************************   */
+    /*  Initialization Section of code                                      */
+    /*  *****************************************************************   */
+
+
+    /**
+     * This method is called automatically when the LWC is ready to be
+     * initialized.  It calls, in sequence, the methods used to create
+     * both the data shown in the UI as well as backing data.
+     */
+    connectedCallback() {
+        this.parseParams();
+        this.parseHeaders();
+        this.generateData();
+    }
+
+
+    /**
+     * Get the parameters for the LWC which have been pased in from the OmniScript as metadata
+     * The parameters are passed in the definition of the element, and are held in an unordered
+     * array called customAttributes.  Each element of the array is an object which has a key:value
+     * pair.  The key is called "name", while the value is called "source".
+     * 
+     * We loop through the array, comparing the name to things we're looking for and, when matched,
+     * saving the value into a module level variable for reference elsewhere. 
+     */
+
+    parseParams() {
+
+        let parms = this.omniJsonDef.propSetMap.customAttributes;
+
+        //  Find the values in the list (don't want to be fussy about the order)
+        parms.forEach((val) => {
+            if (val.name === "headers") {
+                this.parms_headers = val.source;
+                console.log('Headers = ' + this.parms_headers);
+            }
+
+            if (val.name === 'values') {
+                this.parms_values = val.source;
+                console.log('Values = ' + this.parms_values);
+            }
+
+            if (val.name === 'input') {
+                this.parms_input = val.source;
+                console.log('Input @ ' + this.parms_input);
+            }
+
+            if (val.name === 'select') {
+                // Note that select defaults to true
+                let s = val.source.toUpperCase();
+                if (s === 'F' || s === 'FALSE' || s === 'N' || s === "NO") {
+                    this.parms_showSelect = false;
+                }
+                console.log('Select = ' + String(this.parms_showSelect));
+            }
+
+        });
+
+    }
+
+    /**
+     * Retrieve the titles (headers) of the columns.  
+     * 
+     * We expect the custom parameter "headers" to hold the titles in a simple
+     * comma-separated list.
+     */
+
+    parseHeaders() {
+        (this.parms_headers.split(',')).forEach((item, i) => {
+            var x = {};
+            x.key = i;
+            x.value = item.trim();  // Don't want leading / trailing spaces
+            this.headers.push(x);
+        });
+        console.log('Headers: ' + JSON.stringify(this.headers));
+    }
+
+    /**
+     * Generates the data portion of the table
+     */
+    generateData() {
+
+        //  Get the names of the values to displau
+        let values_list = this.parms_values.split(',');
+
+        let tempRows = JSON.parse(JSON.stringify(this.omniJsonData[this.parms_input]));
+
+
+
+        tempRows.forEach((item, i) => {
+            item.tagname = "cb_" + i;
+            item.rowid = i;
+            item.checked = false;
+            item.filtered = false;
+            item.columns = [];
+            values_list.forEach((name,i2) => {
+                let c = {}
+                c.key = i2;
+                c.value = item[name];
+                item.columns.push(c);
+            });
+            this.displayRows.push(item);
+        });
+
+        // use this code for 107.1 and above
+        this.vals = this.omniJsonData[this.parms_input];
+        //this.cols = this.omniJsonData[this.omniJsonDef.name].cols;
+        // use this code for 107 and below
+        //this.vals = this.omniSeedJson[this.omniJsonDef.name].vals;
+        //this.cols = this.omniSeedJson[this.omniJsonDef.name].cols;      
+
+        // console.log("omniJsonData");
+        // console.log(JSON.stringify(this.omniJsonData));
+        // console.log("omniJsonDef");
+        // console.log(JSON.stringify(this.omniJsonDef));
+        // console.log("omniSeedJson");
+        // console.log(JSON.stringify(this.omniSeedJson));
+        // //console.log(JSON.stringify(this.vals));
+        // //console.log(JSON.stringify(this.cols));
+        console.log('Display Rows: ');
+        console.log(JSON.stringify(this.displayRows));
+    }
+
+
+    /*  *****************************************************************   */
+    /*  Runtime Section of code                                             */
+    /*  *****************************************************************   */
+
+
+    filterSelectedFromList(list) {
+        const filteredList = list.filter(
+            // eslint-disable-next-line no-undef
+            checked = true
+        );
+        return filteredList
+    }
+
+    selectRow(event) {
+        const selected = event.target.checked;
+        const target = event.target.name;
+        // eslint-disable-next-line radix
+        const iteration = parseInt(target.substring(3, target.length));
+        this.displayRows[iteration].checked = selected;
+    }
+
+    getRows() {
+        let data = [];
+        let filtered = [];
+        data = this.displayRows;
+        filtered = data.filter(function (item) {
+            return item.checked === true;
+        });
+        return filtered;
+    }
+
+    handleSelection(event) {
+        this.selectRow(event);
+        this.omniUpdateDataJson(this.getRows());
+    }
+}
